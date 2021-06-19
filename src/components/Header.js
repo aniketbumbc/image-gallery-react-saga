06dca79c4243b878a46f691dbe8240386ba750fa
aniket <aniket.umbc@gmail.com>
2021-06-19 17:06:54 +0530

    image gallery added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <h2>Splash Gallery</h2>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import '../css/style.css';
+
+function ImageGrid() {
+  const key =
+    '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  const getPhotos = async () => {
+    debugger;
+    const result = await fetch(
+      `https://api.unsplash.com/photos/?client_id=${key}&per_page=28`
+    );
+    result.json().then((images) => {
+      setPhotos(images);
+    });
+  };
+
+  return (
+    <>
+      <div className='content'>
+        <section className='grid'>
+          {photos.map((image) => (
+            <div
+              key={image.id}
+              className={`item item-${Math.ceil(image.height / image.width)}`}
+            >
+              <img src={image.urls.small} alt={image.user.username} />
+            </div>
+          ))}
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default ImageGrid;
